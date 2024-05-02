6df34e97126522b375a4da2a7fabee3063626b32
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2024-05-01 22:23:54 -0400

    Update predictor.py

--- src/predictor.py
+++ src/predictor.py
@@ -1,1 +1,6 @@
 """Module for including classes in the predictor for heart disease."""
+
+class IsHeartDisease:
+  def predict_heart(self, patient):
+    model = joblib.load('20_outcomes/random_forest_model.pkl')
+    predictions = model.predict(patient)
