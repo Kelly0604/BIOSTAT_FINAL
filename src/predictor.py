41d79814e78f530f7262486d2874de5f1b2ea841
cassiekang <xk10@duke.edu>
2024-05-01 23:29:09 -0400

    add test_predictor.py

--- src/predictor.py
+++ src/predictor.py
@@ -1,21 +1,36 @@
 """Module for including classes in the predictor for heart disease."""
+
+from typing import Any, List
+
 import joblib
 
-class IsHeartDisease:
-    def __init__(self, model_path: str = '20_outcomes/random_forest_model.pkl'):
-        self.model_path = model_path
-        self.model = self.load_model()
 
-    def load_model(self):
+class IsHeartDisease:
+    """Class for predicting heart disease."""
+
+    def __init__(
+        self, model_path: str = "20_outcomes/random_forest_model.pkl"
+    ) -> None:
+        """Initialize the IsHeartDisease class."""
+        self.model_path: str = model_path
+        self.model: Any = self.load_model()
+
+    def load_model(self) -> Any:
+        """Load the machine learning model."""
         try:
             model = joblib.load(self.model_path)
             return model
-        except FileNotFoundError:
-            raise FileNotFoundError(f"The model file was not found at {self.model_path}.")
+        except FileNotFoundError as e:
+            raise FileNotFoundError(
+                f"The model file was not found at {self.model_path}."
+            ) from e
         except Exception as e:
-            raise Exception(f"Failed to load the model due to an error: {e}")
+            raise Exception(
+                f"Failed to load the model due to an error: {e}"
+            ) from e
 
-    def predict_heart(self, patient):
+    def predict_heart(self, patient: List[float]) -> str:
+        """Predict if the patient has heart disease."""
         predictions = self.model.predict([patient])
         predicted_class = predictions[0]
         if predicted_class == 1:
