ca899c5848d674c4657d98c24d8749fb8ae0939b
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2024-05-01 22:34:02 -0400

    Update predictor.py

--- src/predictor.py
+++ src/predictor.py
@@ -1,11 +1,24 @@
 """Module for including classes in the predictor for heart disease."""
+import joblib
 
 class IsHeartDisease:
-  def predict_heart(self, patient):
-    model = joblib.load('20_outcomes/random_forest_model.pkl')
-    predictions = model.predict(patient)
-    predicted_class = predictions[0]
-    if predicted_class == 1:
-            return "The patient is likely diagnosed of heart disease."
+    def __init__(self, model_path: str = '20_outcomes/random_forest_model.pkl'):
+        self.model_path = model_path
+        self.model = self.load_model()
+
+    def load_model(self):
+        try:
+            model = joblib.load(self.model_path)
+            return model
+        except FileNotFoundError:
+            raise FileNotFoundError(f"The model file was not found at {self.model_path}.")
+        except Exception as e:
+            raise Exception(f"Failed to load the model due to an error: {e}")
+
+    def predict_heart(self, patient):
+        predictions = self.model.predict([patient])
+        predicted_class = predictions[0]
+        if predicted_class == 1:
+            return "The patient is likely diagnosed with heart disease."
         else:
-            return "The patient is not likely diagnosed of heart disease."
+            return "The patient is not likely diagnosed with heart disease."
